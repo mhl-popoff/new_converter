59a345acc1d0c002d033d310f58ec0cf90227c22
Никита Шинков <shinkovn@yandex.ru>
2023-06-23 23:12:58 +0400

    robert_integrated

--- src/fileupload/pdf_processing.py
+++ src/fileupload/pdf_processing.py
@@ -1,15 +1,24 @@
-# Импорт PyMuPDF для модуля Python
-import fitz
+import PyPDF2
+from fuzzywuzzy import fuzz
 
-def process_pdf(pdf_document):
-    output_file = "text/output.txt"
+def process_pdf(pdf_document, search_query):
+    results = []
 
-    doc = fitz.open(pdf_document)
+    with open(pdf_document, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
 
-    # Извлекаем и сохраняем текст в файле TXT
-    with open(output_file, "w", encoding="utf-8") as stream:
-        for page in doc:
-            page_text = page.get_text("text").replace('\xa0', ' ')
-            stream.write(page_text)
-             
-             
+
+        for page_number, page in enumerate(reader.pages, start=1):
+            text = page.extract_text()
+            text = text.replace('\n', ' ').replace('\t', ' ').replace('  ', ' ')
+
+            # Вычисление сходства строки поиска с текстом страницы
+            match_score = fuzz.partial_ratio(search_query.lower(), text.lower())
+
+            # Если сходство выше 50%, добавить результат в список
+            if match_score >= 81:
+                results.append((page_number, match_score, 'Совпадение'))
+            elif 81 > match_score >= 65:
+                results.append((page_number, match_score, 'Внимание, необходимо проверить!'))
+
+    return results, search_query
--- src/fileupload/views.py
+++ src/fileupload/views.py
@@ -6,10 +6,18 @@ from .pdf_processing import process_pdf
 def home(request):
     """Функция загрузки pdf-файла."""
     if request.method == "POST":
-        file2 = request.FILES["file"]
-        document = FilesUpload.objects.create(file=file2)
-        document.save()
-        file_path = document.file.path
-        process_pdf(file_path)
-        return HttpResponse("Файл загружен")
+        files = request.FILES.getlist("files")
+        search_query = request.POST.get('search_query')
+        for file in files:
+            document = FilesUpload.objects.create(file=file)
+            document.save()
+            file_path = document.file.path
+            results, search_query = process_pdf(file_path, search_query)
+            output_file = "search_results.txt"
+            with open(output_file, 'w') as output:
+                output.write(f"Объект капитального строительства: {search_query}\n")
+                for i, result in enumerate(results, start=1):
+                    page_number, match_score, comment = result
+                    output.write(f"{i}. Найдено совпадение на странице {page_number} - процент сходства {match_score}% - {comment}\n")
+        return HttpResponse("Files uploaded")
     return render(request, "index.html")
