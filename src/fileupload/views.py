9ffeaed45c2e573dbc0ab439aeef021f8ac04208
Никита Шинков <shinkovn@yandex.ru>
2023-06-24 01:57:06 +0400

    everything works but slowly

--- src/fileupload/views.py
+++ src/fileupload/views.py
@@ -1,4 +1,6 @@
+import os
 from django.shortcuts import render, HttpResponse
+
 from .models import FilesUpload
 from .pdf_processing import process_pdf
 
@@ -8,16 +10,22 @@ def home(request):
     if request.method == "POST":
         files = request.FILES.getlist("files")
         search_query = request.POST.get('search_query')
+        results_dir = "results"
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
         for file in files:
             document = FilesUpload.objects.create(file=file)
             document.save()
             file_path = document.file.path
             results, search_query = process_pdf(file_path, search_query)
-            output_file = "search_results.txt"
+            output_file = os.path.join(results_dir, f"{file.name}_search_results.txt")
             with open(output_file, 'w') as output:
                 output.write(f"Объект капитального строительства: {search_query}\n")
-                for i, result in enumerate(results, start=1):
-                    page_number, match_score, comment = result
-                    output.write(f"{i}. Найдено совпадение на странице {page_number} - процент сходства {match_score}% - {comment}\n")
+                if not results:
+                    output.write("В этом документе совпадений не найдено.\n")
+                else:
+                    for i, result in enumerate(results, start=1):
+                        page_number, match_score, comment = result
+                        output.write(f"{i}. Найдено совпадение на странице {page_number} - процент сходства {match_score}% - {comment}\n")
         return HttpResponse("Files uploaded")
     return render(request, "index.html")
